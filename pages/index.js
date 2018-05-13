a6d1c6cd786b472a4e4b5d35ce7ca2055efb330c
matthi <mruhland01@gmail.com>
2018-05-13 11:45:05 -0700

    got initial data fetch working (existing campaigns) / added semantic ui

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
+
+  //NOTE: componentDidMount() does not get executed by Next server
+  // async componentDidMount() {
+  //   const campaigns = await factory.methods.getDeployedCampaigns().call();
+  // }
+
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
+
+    return { campaigns: campaigns };
   }
 
   render() {
-    return <div>campaigns</div>;
+    return <div>campaigns: {this.props.campaigns[0]}</div>;
   }
 }
 
