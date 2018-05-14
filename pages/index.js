b9aafc62766bed13f6d6e9dbc041de87cddf4c7f
matthi <mruhland01@gmail.com>
2018-05-14 15:18:22 -0700

    further work on layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from "semantic-ui-react";
+
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
-
   //NOTE: componentDidMount() does not get executed by Next server
   // async componentDidMount() {
   //   const campaigns = await factory.methods.getDeployedCampaigns().call();
@@ -16,25 +17,35 @@ class CampaignIndex extends Component {
   }
 
   renderCampaigns() {
-    const items = this.props.campaigns.map((address) => {
-
+    const items = this.props.campaigns.map(address => {
       return {
         header: address,
         description: <a href="#">View Campaign</a>,
         fluid: true
       };
-
     });
 
-    return <Card.Group items={items} />
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
-      <div>
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-        {this.renderCampaigns()}
-      </div>
+      <Layout>
+        <div>
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+          />
+          <h3>Open Campaigns</h3>
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          />
+          {this.renderCampaigns()}
+        </div>
+      </Layout>
     );
   }
 }
