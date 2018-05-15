d0dce06ccd3edeb02ab6c2312fa353112fc5c8a4
matthi <mruhland01@gmail.com>
2018-05-14 21:24:38 -0700

    got the request submittal and retrieval working

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 
 import Layout from "../../../components/Layout";
 import { Link } from "../../../routes";
+import Campaign from "../../../ethereum/campaign";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const campaignAddress = props.query.address;
+    const campaignInstance = Campaign(campaignAddress);
+    const requestCount = await campaignInstance.methods
+      .getRequestCount()
+      .call();
 
-    return { campaignAddress: campaignAddress };
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaignInstance.methods.requests(index).call();
+        })
+    );
+
+    return { campaignAddress: campaignAddress, requests: requests };
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <h3>Requests</h3>
@@ -20,6 +35,19 @@ class RequestIndex extends Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
       </Layout>
     );
   }
--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -10,7 +10,9 @@ class RequestNew extends Component {
   state = {
     value: "",
     description: "",
-    recipient: ""
+    recipient: "",
+    loading: false,
+    errorMessage: ""
   };
 
   static async getInitialProps(props) {
@@ -18,11 +20,36 @@ class RequestNew extends Component {
     return { address };
   }
 
+  onSubmitHandler = async event => {
+    event.preventDefault();
+
+    const campaignInstance = Campaign(this.props.address);
+    const { description, value, recipient } = this.state;
+
+    this.setState({ loading: true, errorMessage: "" });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaignInstance.methods
+        .createRequest(description, web3.utils.toWei(value, "ether"), recipient)
+        .send({ from: accounts[0] });
+
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
+
   render() {
     return (
       <Layout>
+        <Link route={`/campaigns/${this.props.address}/requests`}>
+          <a>Back</a>
+        </Link>
         <h3>Create a Request</h3>
-        <Form>
+        <Form onSubmit={this.onSubmitHandler} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Description</label>
             <Input
@@ -50,7 +77,10 @@ class RequestNew extends Component {
               }
             />
           </Form.Field>
-          <Button primary>Create!</Button>
+          <Message error header="Oops!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>
+            Create!
+          </Button>
         </Form>
       </Layout>
     );
